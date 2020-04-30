217b84e1ffa383d465c619c2f9ea7c05f70ce1f4
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-04-30 22:02:21 +0200

    454 ActiveMqEmbeddedServer configuration

---
+++ src/main/java/pl/kukla/krzys/jms/jms/JmsEmbeddedServer.java
@@ -0,0 +1,27 @@
+package pl.kukla.krzys.jms.jms;
+
+import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
+import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.ActiveMQServers;
+import org.springframework.boot.CommandLineRunner;
+
+/**
+ * @author Krzysztof Kukla
+ */
+public class JmsEmbeddedServer implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        ActiveMQServer activeMQServer = configureJmsServer();
+        activeMQServer.start();
+    }
+
+    private ActiveMQServer configureJmsServer() throws Exception {
+        return ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+            .setPersistenceEnabled(false)
+            .setJournalDirectory("target/data/journal")
+            .setSecurityEnabled(false)
+            .addAcceptorConfiguration("invm", "vm://0")
+        );
+    }
+
+}
